d8c52920f567ad78dc1f9897e3b72c7260a0b26b
David Flores <drfloresj@gmail.com>
2020-06-03 22:01:09 -0700

    built

---
+++ client/build/precache-manifest.eba4f42edb8793c95cbf60dcc5e7d8dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fc4d694d58be1f0833c",
+    "url": "/static/css/main.1892ea1a.chunk.css"
+  },
+  {
+    "revision": "5fc4d694d58be1f0833c",
+    "url": "/static/js/main.6d8a8d4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "219f29db5f10382fa35f",
+    "url": "/static/js/2.600514d9.chunk.js"
+  },
+  {
+    "revision": "32114cb6a7a2356ab3cbbf0808512f02",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://broken-guitar:totallyrandom2020!@ds161209.mlab.com:61209/heroku_q53jtczd");
 
 // Start the API server
 app.listen(PORT, function() {
